c63a767c8fa13b7de6644ce99d88611af8e5f099
Idlaviv17 <jose.javier.angulo.cazarez@gmail.com>
2025-04-17 23:19:34 -0600

    feat(customer): implement the customer service

---
+++ services/customer/src/main/java/com/jangulo/ecommerce/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.jangulo.ecommerce.customer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+}
